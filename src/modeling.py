0de9cea6f21c8841ddb474e852497b2b4a98bddd
Ammar11232 <infosysammar1@gmail.com>
2025-12-12 19:40:53 +0300

    Update modeling.py

--- src/modeling.py
+++ src/modeling.py
@@ -1,1 +1,59 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from tensorflow.keras import layers, models
+
+#Baseline model
+
+lr_model = LogisticRegression(max_iter=3000, random_state=42)
+lr_model.fit(X_train, y_train)
+y_val_pred = lr_model.predict(X_val)
+
+print("Validation Accuracy:", accuracy_score(y_val, y_val_pred))
+print("\nClassification Report (Validation):")
+print(classification_report(y_val, y_val_pred))
+
+#Traditional machine learning model (Support Vector Machine)
+svm_model = SVC(kernel='linear', C=1.0, random_state=42)
+svm_model.fit(X_train, y_train)
+
+y_val_pred_svm = svm_model.predict(X_val)
+print("SVM Validation Accuracy:", accuracy_score(y_val, y_val_pred_svm))
+print(classification_report(y_val, y_val_pred_svm))
+models['SVM'] = svm_model
+
+#Traditional machine learning model (Random Foreset)
+rf_model = RandomForestClassifier(
+    n_estimators=200,
+    max_depth=None,
+    random_state=42,
+    n_jobs=-1)
+rf_model.fit(X_train, y_train)
+
+y_val_pred_rf = rf_model.predict(X_val)
+print("Random Forest Validation Accuracy:", accuracy_score(y_val, y_val_pred_rf))
+print(classification_report(y_val, y_val_pred_rf))
+models['RandomForest'] = rf_model
+
+#Deep learning models
+ffnn_model = models.Sequential([
+    layers.Input(shape=(X_train_emb.shape[1],)),
+    layers.Dense(256, activation="relu"),
+    layers.BatchNormalization(),
+    layers.Dropout(0.3),
+    layers.Dense(128, activation="relu"),
+    layers.BatchNormalization(),
+    layers.Dropout(0.3),
+    layers.Dense(1, activation="sigmoid")])
+
+ffnn_model.compile(
+    optimizer="adam",
+    loss="binary_crossentropy",
+    metrics=["accuracy"])
+ffnn_model.summary()
+
+
+
+
 
